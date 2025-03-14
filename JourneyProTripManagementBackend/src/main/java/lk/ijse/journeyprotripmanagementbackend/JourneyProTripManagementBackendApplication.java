ca73df8748dc99f3a022ed9be66f93a110c8e25e
chathura <chathuhiru45@gmail.com>
2025-03-14 09:38:06 +0530

    create modules

---
+++ JourneyProTripManagementBackend/src/main/java/lk/ijse/journeyprotripmanagementbackend/JourneyProTripManagementBackendApplication.java
@@ -0,0 +1,12 @@
+package lk.ijse.journeyprotripmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyProTripManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneyProTripManagementBackendApplication.class, args);
+    }
+}
