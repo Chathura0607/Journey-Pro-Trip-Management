401c533fec4bb08f1af97856226ed7c977403439
chathura <chathuhiru45@gmail.com>
2025-03-16 04:28:30 +0530

    add un versioned files

--- JourneyProTripManagementBackend/src/main/java/lk/ijse/journeyprotripmanagementbackend/JourneyProTripManagementBackendApplication.java
+++ JourneyProTripManagementBackend/src/main/java/lk/ijse/journeyprotripmanagementbackend/JourneyProTripManagementBackendApplication.java
@@ -1,12 +1,18 @@
 package lk.ijse.journeyprotripmanagementbackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JourneyProTripManagementBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JourneyProTripManagementBackendApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
